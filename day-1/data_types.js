e6575ec0c0a8b944e71d4ac854e4595698dd5f9b
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-05-19 16:16:21 +0600

    1.Let’s talk to machine with it’s own language

---
+++ day-1/data_types.js
@@ -0,0 +1,18 @@
+
+// Data Types  => 
+//                    Simple Data Type / Primitive Data Type
+//                    Complex Data Type / Reference Data Type
+
+
+/******************************************************************************************************
+ *                              Simple Data Type / Primitive Data Type
+ *******************************************************************************************************/
+// Number
+// String
+// Boolean
+// null or undefined
+// Symbol
+
+
+
+
