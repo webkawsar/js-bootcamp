b38bddcbf260f6bc21111f6e6439bb9ff0e05db2
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-05-19 17:23:19 +0600

    6.Exercise -Data type done

---
+++ day-1/exercise.js
@@ -0,0 +1,18 @@
+
+const about = "Karim is a bad boy";
+// console.log(about);
+
+const secondAbout = "Karim is a 'bad' boy";
+// console.log(secondAbout);
+
+const num1 = 420;
+// console.log(num1); // data type number
+
+const num2 = '420';
+// console.log(num2);// data type string cause we are used single quation.
+
+const myName = 'This is Kawsar Ahmed. ';
+const age = 'I am 26 years old. ';
+const profession = 'I am a Junior Full Stack Developer. ';
+const favorite = 'I love "JavaScript" cause can\'t learn other language. ';
+console.log(myName + age + profession + favorite);
