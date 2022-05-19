1c3473ff7cd9a1c0cf95c74887f47142f9cce455
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-05-19 16:43:13 +0600

    3.JavaScript Primitive-Number

---
+++ day-1/number.js
@@ -0,0 +1,11 @@
+
+
+// console.log(3 + 3);
+
+// console.log( 100 % 30);
+
+// console.log(typeof null); // Object
+
+// console.log(typeof undefined); // undefined
+
+// console.log(typeof Symbol); // function
