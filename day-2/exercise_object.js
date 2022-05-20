622931bc1ceb8b6a1851080918ff6da7e11aa4ef
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-05-20 09:48:01 +0600

    13.Exercise-object

--- day-2/exercise_object.js
+++ day-2/exercise_object.js
@@ -23,3 +23,43 @@ demoProfile[prop] = 'red'; // we can store value
 
 // console.log(demoProfile['is married']); // access invalid key value
 
+
+
+// const profile = {
+//     firstName: 'Shamim',
+//     lastName: 'Mohammad',
+//     age: 25,
+//     profession: 'Pilot',
+//     place: 'Shamsher Nagar Biman Ghati',
+//     'is Married': false
+// }
+
+
+
+const movies = [
+    {
+        id: 101,
+        movieName: 'Jani na',
+        rating: 8.9,
+        category: 'fantastic'
+    },
+    {
+        id: 102,
+        movieName: 'Mani na',
+        rating: 9.8,
+        category: 'Romantic'
+    },
+    {
+        id: 103,
+        movieName: 'Chini na',
+        rating: 9.8,
+        category: 'ching chung vung'
+    },
+
+]
+
+// console.log(movies[0].id + movies[0].movieName + movies[0].rating + movies[0].category);
+
+
+const movieIndex = movies[1];
+// console.log(movieIndex.id + movieIndex.movieName + movieIndex.rating + movieIndex.category);
