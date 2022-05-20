67cc7773c9ae18f91f19499887bfb391dae00bf0
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-05-20 08:01:58 +0600

    7.Dynamic typing

---
+++ day-2/dynamic_type.js
@@ -0,0 +1,12 @@
+
+let productCode = 'samsung_led_tv';
+// console.log(typeof productCode); // string
+
+productCode = 420;
+// console.log(typeof productCode); // number
+
+let isAvailable = true;
+// console.log(typeof isAvailable); // boolean
+
+isAvailable = 'yes';
+console.log(typeof isAvailable); // string
