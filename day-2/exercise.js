5c5acc9cfc44285da585fcfa592f4f7ced3f2322
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-05-20 08:38:38 +0600

    10.exercise-Array

---
+++ day-2/exercise.js
@@ -0,0 +1,9 @@
+
+const bio = [];
+bio[0] = 'Imran Ahmed';
+bio[1] = 25;
+bio[2] = 'Graphic Designer';
+bio[3] = 'MoulviBazar';
+
+// console.log(bio);
+console.log('This is ' +  bio[0] + '. Age is ' + bio[1] + '. Profession ' + bio[2] + '. Lives in ' + bio[3]);
