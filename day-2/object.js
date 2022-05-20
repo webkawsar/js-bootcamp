4a15d6c295cdd4fa296312adc956e176c2a7e813
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-05-20 08:56:22 +0600

    11.complex data type -Object

---
+++ day-2/object.js
@@ -0,0 +1,13 @@
+
+const profile = {
+    fName: 'Kawsar',
+    lName: 'Ahmed',
+    age: 25,
+    profession: 'Junior Developer',
+    previousProf: 'Sales Executive'
+}
+
+// console.log(profile.fName);
+// console.log(profile.lName);
+// console.log(profile.previousProf);
+console.log(profile['profession']);
