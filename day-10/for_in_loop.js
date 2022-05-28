bdc305824996bcc4ca8969d7b7355b6894b519f7
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-05-28 22:55:26 +0600

    added 2 files

---
+++ day-10/for_in_loop.js
@@ -0,0 +1,8 @@
+
+
+// For In Loop
+const myInfo = ['Kawsar', 'Ahmed', 26, 'Web Developer', 'Bekar'];
+for (const key in myInfo) {
+    console.log(key); // show object key
+    console.log(myInfo[key]); // show value
+}
---
+++ day-10/for_of_loop.js
@@ -0,0 +1,7 @@
+
+
+
+const myInfo = ['Kawsar', 'Ahmed', 26, 'Web Developer', 'Bekar'];
+for (const value of myInfo) {
+    console.log(value);
+}
