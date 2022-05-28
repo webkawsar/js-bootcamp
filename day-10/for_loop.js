4020b51a7b59b5ffca1918a48492a979a1a5390a
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-05-28 22:44:04 +0600

    added day - 10

---
+++ day-10/for_loop.js
@@ -0,0 +1,9 @@
+
+
+// For Loop
+const students = ['Kawsar', 'Imran', 'Shamim', 'Ajel'];
+for (let i = 0; i < students.length; i++) {
+    const element = students[i];
+    console.log(element);
+}
+
---
+++ day-10/while_loop.js
@@ -0,0 +1,9 @@
+
+// While Loop
+
+let i = 0;
+while (i < 10) {
+    
+    console.log(`code repeat in ${i} times`);
+    i++;
+}
