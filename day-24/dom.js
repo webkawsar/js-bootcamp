7c21bc7bdca12c14cd4d99c486ee745cc74e1ab0
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-06-11 20:59:10 +0600

    added day 24 practice

---
+++ day-24/dom.js
@@ -0,0 +1,30 @@
+
+// console.log(document);
+// console.log(window);
+// console.dir(document)
+
+let value = document;
+value = document.head;
+value = document.title;
+
+// get html collections
+value = document.all;
+
+// get html collections
+value = document.links;
+
+
+console.log(value);
+
+
+
+
+
+
+
+
+
+
+
+
+
