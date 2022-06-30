9135486024a4270fef051d451691a4d9be50fd57
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-06-30 21:42:52 +0600

    added regular expression in javascript

---
+++ day-42/regular_expression_in_javascript.js
@@ -0,0 +1,39 @@
+
+
+
+
+// Regular Expression in JavaScript
+// replaced => string method
+// search => string method
+// test => regular expression method
+// match => string method
+// exec => regular expression method
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
