a56f4c7be8c3b6785e87558b8dff207be9674177
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-07-03 22:34:52 +0600

    added asynchronous programming practice

---
+++ day-41/asynchronus_programming.js
@@ -0,0 +1,48 @@
+
+// Synchronous vs Asynchronous
+// JavaScript is single threaded
+
+// callback and callback hell
+// promise
+// async/await
+
+
+
+
+
+// const functionOne = () => {
+
+//     console.log('Function one');
+//     return 1;
+// }
+
+
+// const functionTwo = () => {
+
+//     console.log("Function two");
+//     return 2;
+// }
+
+
+// const sumFunction = (receivedFunctionOne, receivedFunctionTwo) => {
+
+//    const sum = () => {
+
+//         return receivedFunctionOne() + receivedFunctionTwo();
+//    }
+
+//    return sum;
+// }
+
+// const result = sumFunction(functionOne, functionTwo)
+// console.log(result());
+
+
+
+
+
+
+
+
+
+
---
+++ day-43/rest_api.js
@@ -0,0 +1,34 @@
+
+
+// Rest API Architecture
+
+
+// C - Create => POST           /books
+// R - Read   => GET            /books
+// U - Update => PUT / PATCH    /books/:id
+// D - Delete => DELETE         /books/:id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
