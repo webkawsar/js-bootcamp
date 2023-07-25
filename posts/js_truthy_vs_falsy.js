bcb6f044414350213e5015a0788eff2b5b793d8d
Kawsar <web.kawsarahmed@gmail.com>
2023-07-25 11:14:15 +0600

    minor change

--- posts/js_truthy_vs_falsy.js
+++ posts/js_truthy_vs_falsy.js
@@ -1,52 +1,5 @@
 
 
-
-const profile = {
-    firstName: 'Kawsar',
-    lastName: 'Ahmed',
-    profession: 'Web Developer',
-}
-
-// Dot Notation
-console.log(profile.profession); // Web Developer
-
-// Bracket Notation
-console.log(profile['profession']); // Web Developer
-
-// Dynamic Access
-const pro = 'profession';
-console.log(profile[pro]); // Web Developer
-
-// Object Destructuring
-const { profession } = profile;
-console.log(profession); // Web Developer
-
-
-console.log('firstName' in profile);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /***************************************************** */
 
 
